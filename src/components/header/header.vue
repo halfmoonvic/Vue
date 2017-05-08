68657cec433affdcbee3fe0a9a28ecf0fdb88388
halfmoon <halfmoonvic@gmail.com>
2017-05-08 18:36:31 +0800

    5.08 下班

---
+++ src/components/header/header.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="header">
+        我是 header
+    </div>
+</template>
+
+<script>
+export default {
+
+  name: 'header',
+
+  data () {
+    return {
+
+    };
+  }
+};
+</script>
+
+<style lang="css" scoped>
+</style>
